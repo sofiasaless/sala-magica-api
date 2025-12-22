6341900aade471cb3035e0d24c8fe37d0204f3c0
sofiasaless <sofiasaleswk@gmail.com>
2025-12-22 16:53:01 -0300

    feat: rotas para atualização e exclusão de categorias

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,3 +1,4 @@
+import { deleteCategory } from "../controllers/category.controller";
 import { Category } from "../types/categoria.type";
 import { COLLECTIONS, docToObject } from "../utils/firestore.util";
 import { PatternService } from "./pattern.service";
@@ -18,18 +19,30 @@ export class CategoryService extends PatternService {
 
     const ref = await this.setup().add(toSave);
     const doc = await ref.get()
-    
+
     return docToObject<Category>(doc.id, doc.data()!);
   }
 
   public async findAllCategories(): Promise<Category[]> {
     const docs = (await this.setup().get()).docs
-    
+
     const list = docs.map((doc) => {
       return docToObject<Category>(doc.id, doc.data()!)
     })
 
     return list;
+  }
+
+  public async updateCategory(id_category: string, nome: string) {
+    const ref = this.setup().doc(id_category);
+    await ref.update({
+      nome: nome
+    })
+  }
+
+  public async deleteCategory(id_category: string) {
+    const ref = this.setup().doc(id_category);
+    await ref.delete();
   }
 }
 
